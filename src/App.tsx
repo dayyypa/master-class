9c339bff25792581d0d10b04c4a4e24abffffb5a
Hee Chul <da.yyy.pa@gmail.com>
2024-11-21 16:12:27 +0900

    styled.component 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const Title = styled.h1`
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	height: 100vh;
+	width: 100vw;
+	justify-content: center;
+	align-items: center;
+	background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const rotationAnimation = keyframes`
+	0%{
+		transform: rotate(0deg);
+		border-radius: 0px;
+	} 50%{
+		border-radius: 100px;
+	} 100%{
+		transform: rotate(360deg);
+		border-radius: 0px;
+	}
+`;
+
+const Emoji = styled.span`
+	font-size: 30px;
+`;
+
+const Box = styled.div`
+	height: 200px;
+	width: 200px;
+	background-color: chocolate;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	animation: ${rotationAnimation} 2s linear infinite;
+	${Emoji}:hover {
+		font-size: 98px;
+	}
+`;
 
 function App() {
 	return (
-		<div>
-			<div>111</div>
-			<div>222</div>
-		</div>
+		<Wrapper>
+			<Title>Hi, Im dayul fa</Title>
+		</Wrapper>
 	);
 }
 
