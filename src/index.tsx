a4a84a586221770310ac5b82a39a8a8c07fbf1d2
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-11-25 23:10:07 +0900

    styles setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
-
-// const darkTheme = {
-// 	textColor: 'whitesmoke',
-// 	backgroundColor: '#111'
-// };
-
-// const lightTheme = {
-// 	textColor: '#111',
-// 	backgroundColor: 'whitesmoke'
-// };
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ThemeProvider theme={darkTheme}>
+		<ThemeProvider theme={theme}>
 			<App />
 		</ThemeProvider>
 	</React.StrictMode>
