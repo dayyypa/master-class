329c406b3c6ce0a10daf5f02bb7f8d4397fa561d
Hee Chul <da.yyy.pa@gmail.com>
2024-11-29 16:27:46 +0900

    query / install  한 번 하기

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { fetchCoins } from 'src/api';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -53,7 +55,7 @@ const CoinImg = styled.img`
 	margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
 	id: string;
 	name: string;
 	symbol: string;
@@ -64,28 +66,18 @@ interface CoinInterface {
 }
 
 function Coins() {
-	const [coins, setCoins] = useState<CoinInterface[]>([]);
-	const [loading, setLoading] = useState(true);
+	const { isLoading, data } = useQuery<ICoin[]>('allCoins', fetchCoins);
 
-	useEffect(() => {
-		//coin list api call
-		(async () => {
-			const response = await fetch('https://api.coinpaprika.com/v1/coins');
-			const json = await response.json();
-			setCoins(json.slice(0, 100));
-			setLoading(false);
-		})();
-	}, []);
 	return (
 		<Container>
 			<Header>
 				<Title>코인</Title>
 			</Header>
-			{loading ? (
+			{isLoading ? (
 				<Loader>Loading ...</Loader>
 			) : (
 				<CoinsList>
-					{coins.map((coin) => (
+					{data?.slice(0, 100).map((coin) => (
 						<Coin key={coin.id}>
 							<Link
 								to={{
