07641c78f8ff4611b14ad8a0dbdeeb545846619d
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-12-23 08:07:28 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import React from 'react';
 
 function App() {
-	const onDragEnd = () => {};
-	return (
-		<DragDropContext onDragEnd={onDragEnd}>
-			<div>
-				<Droppable droppableId="one">
-					{(magic) => (
-						<ul ref={magic.innerRef} {...magic.droppableProps}>
-							<Draggable draggableId="first" index={0}>
-								{(magic) => (
-									<li ref={magic.innerRef} {...magic.draggableProps}>
-										<span {...magic.dragHandleProps}>🔥</span>One
-									</li>
-								)}
-							</Draggable>
-							<Draggable draggableId="second" index={1}>
-								{(magic) => (
-									<li ref={magic.innerRef} {...magic.draggableProps}>
-										<span {...magic.dragHandleProps}>🔥</span>Two
-									</li>
-								)}
-							</Draggable>
-						</ul>
-					)}
-				</Droppable>
-			</div>
-		</DragDropContext>
-	);
+	return <div>hello</div>;
 }
 
 export default App;
