57c28f59c9b4fa19461775eb73889c0a04cf6990
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-12-26 22:46:42 +0900

    house

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,7 @@ body {
   font-family: 'Source Sans Pro', sans-serif;
   color:black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#9dca00,#0c6922);
+  background:linear-gradient(135deg,#e09,#d0e);
 }
 a {
   text-decoration:none;
